f65c4939f8e48aa268767df8cb66f50d48969636
Genoos <65106962+Genoos@users.noreply.github.com>
2021-01-31 19:56:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import sys
 import random
 
 pygame.init()
@@ -255,6 +256,8 @@ def menu(death_count):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
+                sys.exit()
             if event.type == pygame.KEYDOWN:
                 main()
 
